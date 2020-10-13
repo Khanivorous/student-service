6b60625d4ce6554b3d4ac48d67c550aa9d8a23aa
Khanivorous <taherul_khan@hotmail.co.uk>
2020-10-13 21:15:45 +0200

    Amended Dockerfile to use jdk 14 and removed unused import

--- src/main/java/com/khanivorous/studentservice/student/StudentRepository.java
+++ src/main/java/com/khanivorous/studentservice/student/StudentRepository.java
@@ -1,6 +1,5 @@
 package com.khanivorous.studentservice.student;
 
-import com.khanivorous.studentservice.student.Student;
 import org.springframework.data.repository.CrudRepository;
 
 public interface StudentRepository extends CrudRepository<Student, Integer> {
