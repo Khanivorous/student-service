b2a1eeaebca48e653b092f7d107f9edc717df47f
Khanivorous <taherul_khan@hotmail.co.uk>
2021-12-13 19:33:13 +0100

    refactored classes into new folder structure

--- src/main/java/com/khanivorous/studentservice/student/services/StudentRepository.java
+++ src/main/java/com/khanivorous/studentservice/student/services/StudentRepository.java
@@ -1,5 +1,6 @@
-package com.khanivorous.studentservice.student;
+package com.khanivorous.studentservice.student.services;
 
+import com.khanivorous.studentservice.student.models.Student;
 import org.springframework.data.repository.CrudRepository;
 
 public interface StudentRepository extends CrudRepository<Student, Integer> {
